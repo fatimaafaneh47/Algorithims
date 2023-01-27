423c64a28cb75c3225864f9826cda16f57e8a0f2
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2023-01-27 19:52:24 +0200

    add

---
+++ Two Sum/leetcode.java
@@ -0,0 +1,14 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+class leetcode {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
